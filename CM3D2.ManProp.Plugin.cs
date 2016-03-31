fed64b2debf43fbf9fb4aede2e7997016c72eaa1
qucucu <qucucu9@gmail.com>
2016-03-31 10:46:41 +0900

    version1.0.0.0完成 メニューサンプル

---
+++ CM3D2.ManProp.Plugin.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityInjector;
+using UnityInjector.Attributes;
+using PluginExt;
+
+namespace CM3D2.ManProp.Plugin
+{
+    [PluginFilter("CM3D2x64"),
+    PluginFilter("CM3D2x86"),
+    PluginFilter("CM3D2VRx64"),
+    PluginFilter("CM3D2OHx64"),
+    PluginName("Man Prop"),
+    PluginVersion("1.0.0.0")]
+    public class ManProp : ExPluginBase
+    {
+        #region Constants
+        class PluginConfig
+        {
+            public string[] manMenu= { "mhead000_i_.menu", "mhead001_i_.menu", "mhead002_i_.menu", "mhead003_i_.menu", "mhead004_i_.menu" };
+        }
+
+        #endregion
+        #region Variables
+
+        private PluginConfig config;
+
+        #endregion
+        #region Override Methods
+
+        public void Awake()
+        {
+            GameObject.DontDestroyOnLoad(this);
+            this.config = ReadConfig<PluginConfig>();
+        }
+
+        public void Start()
+        {
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+            var characterMgr = GameMain.Instance.CharacterMgr;
+            var menus = config.manMenu;
+            for (int number = 1; number < characterMgr.GetManCount(); number++)
+            {
+                var index = number - 1;
+                if (menus.Length <= index)
+                    break;
+                var menu = menus[index];
+
+                var man = characterMgr.GetMan(number);
+                man.SetProp(MPN.head, menu, 0);
+                man.AllProcProp();
+            }
+        }
+        #endregion
+    }
+}
